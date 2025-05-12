81ff2740a39506007bd0f93949ded333628773f0
Vinicius Leandro <111243990+ViniciusLABernardes@users.noreply.github.com>
2025-05-12 20:31:42 -0300

    Add files via upload

---
+++ UWbike/src/main/java/br/com/UWbike/UWbikeApplication.java
@@ -0,0 +1,13 @@
+package br.com.UWbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UWbikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UWbikeApplication.class, args);
+	}
+
+}
---
+++ UWbike/src/test/java/br/com/UWbike/UWbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.UWbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UWbikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
